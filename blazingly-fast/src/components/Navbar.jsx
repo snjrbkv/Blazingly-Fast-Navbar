802f1204452f3b22aaff6a2603b68c123c024d00
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-04-24 07:28:34 +0500

    adding i18 data file

--- blazingly-fast/src/components/Navbar.jsx
+++ blazingly-fast/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+
 import "./Navbar.css";
 const Navbar = () => {
   return (
     <div className="container">
       <div className="navbar">
         <h1>Fast</h1>
+
         <ul>
           <li>
             <Link to="/">Home</Link>
---
+++ blazingly-fast/src/lang/langu.js
@@ -0,0 +1,29 @@
+export const langu = {
+  eng: {
+    navbar: {
+      logo: "Fast",
+      home: "Home",
+      blog: "Blog",
+      contact: "Contact",
+      page: "pages",
+    },
+  },
+  ru: {
+    navbar: {
+      logo: "Легкий логотип",
+      home: "Дом",
+      blog: "Комментарии",
+      contact: "связь",
+      page: "страницы",
+    },
+  },
+  uz: {
+    navbar: {
+      logo: "Oson Logo",
+      home: "Bosh Sahifa",
+      blog: "Sharhlar",
+      contact: "bog'lanish",
+      page: "sahifalar",
+    },
+  },
+};
