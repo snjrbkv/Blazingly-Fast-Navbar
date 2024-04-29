742bc4b8a0b46feb949dac789f0691a154f1829d
MuhammadYusuf <xolmuhammedovv@gmail.com>
2024-04-29 22:46:48 +0500

    adding Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Hello from "./components/Hello";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hello />
       <div>
         <Outlet />
       </div>
